d87800256a62c258a03852dff54372caa55062af
enti-i <familiehanazay@gmail.com>
2025-10-26 20:48:43 +0100

    Load persisted theme before window startup

--- EntiClient/App.xaml.cs
+++ EntiClient/App.xaml.cs
@@ -6,7 +6,7 @@ public partial class App : Application
 {
     protected override void OnStartup(StartupEventArgs e)
     {
-        base.OnStartup(e);
         ThemeManager.LoadPersistedTheme();
+        base.OnStartup(e);
     }
 }
